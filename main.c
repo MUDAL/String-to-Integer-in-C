a6979e1d51820c5005ee2dbf3d218938a8a4113e
Olaoluwa Raji <46250887+MUDAL@users.noreply.github.com>
2021-02-12 09:18:52 +0100

    Update main.c

--- main.c
+++ main.c
@@ -18,6 +18,18 @@ uint32_t Power_Of_10(uint32_t n)
 
 uint32_t String_To_Integer(char* pBuffer)
 {
+  /*
+  each character of the string is converted to
+  an integer and multiplied by the appropriate power
+  of 10. e.g.
+
+  '932':
+  '9' - '0' = 9
+  '3' - '0' = 3
+  '2' - '0' = 2
+
+  9 x 10^2 + 3 x 10^1 + 2 x 10^0 = 932.
+  */
   uint32_t integer = 0;
   uint8_t bufferLength = strlen(pBuffer);
   for (uint8_t i = 0; i < bufferLength; i++)
@@ -29,6 +41,6 @@ uint32_t String_To_Integer(char* pBuffer)
 
 int main()
 {
-    printf("%d\n",String_To_Integer("112987"));
+    printf("%d\n",String_To_Integer("9543"));
     return 0;
 }
